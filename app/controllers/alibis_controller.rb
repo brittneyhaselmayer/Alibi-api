7d14f8d9fce431b4361ba5b6941c82421d9f4c10
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2020-02-03 17:27:09 -0600

    alibi controller

--- app/controllers/alibis_controller.rb
+++ app/controllers/alibis_controller.rb
@@ -1,2 +1,24 @@
 class AlibisController < ApplicationController
+   skip_before_action :authorized, only: [:create, :index]
+def create
+        @alibi = Alibi.create(alibi_params)
+        if @alibi.valid?
+          
+          render json: { alibi: @alibi }, status: :created
+        else
+          render json: { error: 'failed to create alibi' }, status: :not_acceptable
+        end
+      end
+    
+
+      def index
+        alibis = Alibi.all 
+        render json: alibis,
+        include: [:user]
+    end
+ 
+  private
+  def alibi_params
+    params.require(:alibi).permit(:user_id, :when)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+movie= Event.create(occasion: "Movie")
+dinner= Event.create(occasion: "Dinner")
+yoga= Event.create(occasion: "Yoga")
+workout= Event.create(occasion: "Workout")
+lunch= Event.create(occasion: "Lunch")
+hiking= Event.create(occasion: "Hiking")
+brunch= Event.create(occasion: "Brunch")
+workevent= Event.create(occasion: "Work Event")
+
+
+
+
+
